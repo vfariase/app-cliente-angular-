df3db3779fff8ffdff322772ae1308e763ebb823
vfarias <e>
2020-03-04 15:27:46 -0300

    Se agrega libreria de HttpClient, se inyecta clase HttpClient y se modifica método getClientes

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -2,16 +2,24 @@ import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Injectable } from '@angular/core';
 import {Observable,of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndpoint:string='http://localhost:8090/api/clientes';
+  constructor(private httpClient: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
-    return of(CLIENTES);
+    /*Es una forma de retorna el valor de tipo any 
+    return this.httpClient.get<Cliente[]>(this.urlEndpoint);*/
+    return this.httpClient.get(this.urlEndpoint).pipe(
+      map(response=> response as Cliente[])
+    );
+
   }
 }
 
