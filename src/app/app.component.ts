6339f68d7cc425e60bcad64ac0994c2df83f76ce
vfarias <e>
2020-03-04 12:06:37 -0300

    instalaciónde angular, creacionde componentes cliente{el html,el typescripts}, creacionde service de forma local, aún no se prueba con backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
+//app.component.css sería nuestro componente principal o por defecto,
+//a partit de este componente podemos agregar otros Un componente se puede anidar dentro de otro con un componente fijo o bien un componente padre podría
+// estar formado por varios componentes hijos se conoce como el patrón de diseño composites o compositor.
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'Bienvenido a angular';
+
+  curso ='Curso Spring 5 con angular 7';
+  profesor: string = 'Victor Farías Estay'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { ClienteService } from './clientes/cliente.service';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { from } from 'rxjs';
+import { ClientesComponent } from './clientes/clientes.component';
+import { DirectivaComponent } from './directiva/directiva.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ClientesComponent,
+    FooterComponent,
+    DirectivaComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ClienteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
