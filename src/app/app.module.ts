aa158ab2cde83f9f9e1d64ad71a01bdc0338bfb2
vfarias <e>
2020-03-10 15:15:30 -0300

    Se agrega  modulo FormsModule, luego a los Imports de los modulos y tambien cabe recordar que se incorpora la ruta 'clientes/form/:id'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ const routes:Routes=[
   {path:'',redirectTo:'/clientes',pathMatch:'full'},
   {path:'directivas',component:DirectivaComponent},
   {path:'clientes',component:ClientesComponent},
-  {path:'clientes/form',component:FormComponent}
+  {path:'clientes/form',component:FormComponent},
+  {path:'clientes/form/:id',component:FormComponent}
 ]
 
 
@@ -33,9 +34,10 @@ const routes:Routes=[
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes),
     HttpClientModule,
-    FormsModule
+    
   ],
   providers: [ClienteService],
   bootstrap: [AppComponent]
