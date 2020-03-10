8ffaf8fc2d3c5964b151f559fe5b9735171f6f5e
vfarias <e>
2020-03-10 15:32:56 -0300

    Se modifica atributo de createAt a fecha

--- src/app/clientes/clientes.json.ts
+++ src/app/clientes/clientes.json.ts
@@ -1,5 +1,5 @@
 import {Cliente} from './cliente';
 
-export const CLIENTES:Cliente[]=[{id:1,nombre:'Victor',apellido:'Farias',createAt:'17-12-2018',email:'vfariase@gmail.com'}
-,{id:2,nombre:'Javier',apellido:'Farias',createAt:'05-08-2017',email:'javierfariasestay@gmail.com'}
-,{id:3,nombre:'Matías',apellido:'Rozas',createAt:'05-08-2017',email:'mrozas@gmail.com'}];
+export const CLIENTES:Cliente[]=[{id:1,nombre:'Victor',apellido:'Farias',fecha:'17-12-2018',email:'vfariase@gmail.com'}
+,{id:2,nombre:'Javier',apellido:'Farias',fecha:'05-08-2017',email:'javierfariasestay@gmail.com'}
+,{id:3,nombre:'Matías',apellido:'Rozas',fecha:'05-08-2017',email:'mrozas@gmail.com'}];
