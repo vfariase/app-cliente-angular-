389fff7c7524b6ff1330b4d635b91e1dcb386deb
vfarias <e>
2020-03-18 20:35:13 -0300

    se agrega manejo de error

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,9 +1,14 @@
-import { CLIENTES } from './clientes.json';
+import  swal  from 'sweetalert2';
+//import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { Injectable } from '@angular/core';
-import {Observable,of} from 'rxjs';
+import {Observable,of,throwError} from 'rxjs';
 import {HttpClient,HttpHeaders} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import {map,catchError} from 'rxjs/operators';
+
+
+//catchError se encarga de interceptar el observable  en busca de fallas.
+
 
 @Injectable({
   providedIn: 'root'
@@ -25,13 +30,42 @@ export class ClienteService {
 
   create(cliente: Cliente): Observable<Cliente[]>{
     return this.httpClient.post(this.urlEndpoint,cliente,{headers:this.httpHeaders}).pipe(
-      map(response =>response as Cliente[])
-    )
-
+      map(response =>response as Cliente[])  
+      ).pipe(catchError( e=>{
+                      
+        return throwError(e);
+      })
+      )
   }
 
   getCliente(id):Observable<Cliente>{
-      return this.httpClient.put<Cliente>(`this.urlEndpoint+${id}`,{headers:this.httpHeaders})
+      return this.httpClient.get<Cliente>(`${this.urlEndpoint}/${id}`,{headers:this.httpHeaders}).
+      pipe(catchError(e => {
+         console.error(e.error.mensaje);
+         swal.fire('Error al obtener al cliente',e.error.mensaje,'error');            
+         return throwError(e);
+      }))
   }
+
+
+  update(cliente:Cliente):Observable<Cliente>{
+    
+    return this.httpClient.put<Cliente>(`${this.urlEndpoint}/${cliente.id}`,cliente,{headers:this.httpHeaders})
+    .pipe(catchError(e =>{
+      console.error(e.error.mensaje);
+      swal.fire('Error al modificar',e.error.mensaje,'error');            
+      return throwError(e);      
+    }));
+}
+
+  delete(id:Number):Observable<Cliente>{
+    return this.httpClient.delete<Cliente>(`${this.urlEndpoint}/${id}`,{headers:this.httpHeaders})
+    .pipe(catchError(e => {
+      console.error(e.error.mensaje);
+      swal.fire('Error al eliminar',e.error.mensaje,'error');            
+      return throwError(e);
+      }));
+  }
+
 }
 
