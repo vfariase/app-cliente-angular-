2bdd858e20b556d1717a8b84e919f03cf704bf77
vfarias <e>
2020-04-01 14:17:19 -0300

    se modifica el contenido del método gOnInit(), (activatedRoute)que se encarga de obtener el parametro de la pagina paginador.component.html y se encarga de listar los clientes correspondientes a esa pagina

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,9 +1,12 @@
+import { ActivatedRoute } from '@angular/router';
 import { ClienteService } from './cliente.service';
 import { CLIENTES } from './clientes.json';
 import { Component, OnInit} from '@angular/core';
 import {Cliente} from './cliente';
 import swal from 'sweetalert2';
 import {tap} from 'rxjs/operators';
+
+
 
 @Component({
   selector: 'app-clientes',
@@ -13,24 +16,35 @@ import {tap} from 'rxjs/operators';
 export class ClientesComponent implements OnInit{
  
    clientes : Cliente[];
-  
-  constructor(private clienteService:ClienteService) { }
+   paginador : any;
+  constructor(private clienteService:ClienteService, private activatedRoute:ActivatedRoute) { }
  
   ngOnInit(){
-    this.clienteService.getClientes().pipe(
-      tap(clientes =>{
+
+   
+    this.activatedRoute.paramMap.subscribe(params =>{
+      let page:number = +params.get('page');
+      console.log('inicio de metodo page :'+page);
+      if(!page){
+        page =0;
+      }
+    
+    this.clienteService.getClientes(page).pipe(
+      tap(response =>{
         console.log("clientesComponent tap:3");
-         clientes.forEach(cliente => {
-           console.log("Clientes : "+cliente);
+        console.log("page :"+page);
+           (response.content as Cliente[]).forEach(cliente => {
+           console.log("Clientes : "+cliente.nombre);
          });
       })
     ).subscribe(
-          (clientes)=>{
-         this.clientes=clientes
-         
+         response=>{
+           console.log("En el susbcribe");
+           this.clientes=response.content as Cliente[];
+           this.paginador=response;
       }
     );
-  }
+  })}
 
 
   delete(cliente:Cliente){
